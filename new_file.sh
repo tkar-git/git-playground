379cf1c70e1caf8503c5cb8fd0144a7e68dd0e6b
Tamasi Kar <tamasi.kar@gmail.com>
2019-05-30 14:34:11 +0200

    new test script

---
+++ new_file.sh
@@ -0,0 +1,8 @@
+####test script#####
+echo I am adding this file just for fun
+
+echo Actually this is not quite true..
+
+echo I want to see what happens when I fetch and checkout an existing remote branch into a new pc and make changes
+
+
