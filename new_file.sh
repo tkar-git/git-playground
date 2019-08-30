acb22291af384c7493f4408e5cf594f49597c8df
Tamasi Kar <tamasi.kar@gmail.com>
2019-08-30 14:41:18 +0200

    removed new file

--- new_file.sh
+++
@@ -1,8 +0,0 @@
-####test script#####
-echo I am adding this file just for fun
-
-echo Actually this is not quite true..
-
-echo I want to see what happens when I fetch and checkout an existing remote branch into a new pc and make changes
-
-
